5c642d7895186c9cb163c9948cf7ca7c7ce96515
Dmitry Novikov <dnovikov@me.com>
2022-05-12 21:59:55 +0300

    vector typedef's added

---
+++ vector/TestVector.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+#if 1
+	#include <vector>
+	namespace ft = std;
+#else
+	#include "vector.hpp"
+#endif
+
+// class without public constructor
+class Test {
+	private:
+		unsigned 		_id;
+		int     		_int;
+		std::string		_string;
+		Test() {};
+
+	public:
+		Test(unsigned id): _id(id) {
+			if (id == 42)
+				throw "ERROR DATA";
+			if (_id % 2)
+				_int = -_id;
+			else
+				_int = _id;
+			_string = std::to_string(_int).append("_tester");
+		};
+
+		Test(char chr) {
+			if (chr == 'e')
+				throw "ERROR CONSTRUCTION";
+		}
+
+		~Test() {
+//			(void)_id;
+		};
+
+		unsigned 	getId() const {
+			return _id;
+		}
+
+		int			getInt() const {
+			return _int;
+		}
+
+		std::string	getString() const {
+			return _string;
+		}
+
+		bool	operator== (const Test &value) const {
+			bool result = false;
+			if (_id == value.getId()
+				&& _int == value.getInt()
+				&& !_string.compare(value._string))
+				result = true;
+			return result;
+		}
+};
+
+std::ostream &operator<<(std::ostream &out, const Test &value)
+{
+	out << value.getId() << " | " << value.getInt() << " | " << value.getString();
+	return out;
+}
+
+int main() {
+	Test t1((unsigned)1);
+	Test t2((unsigned)3);
+
+		std::cout << (t1 == t2) << std::endl;
+//		ft::vector<Test> vector;
+//
+//		for (int i = 0; i < 11; i++) {
+//			Test    testData(i + 21);
+//			vector.push_back(testData);
+//		}
+
+}
---
+++ vector/vector.hpp
@@ -0,0 +1,27 @@
+#pragma once;
+
+#include <memory>
+#include <iterator>
+namespace ft
+{
+	template<class T, class Allocator = std::allocator <T> >
+	class vector {
+	public:
+		typedef T											value_type;
+		typedef Allocator									allocator_type;
+		typedef typename allocator_type::reference     	 	reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer         	pointer;
+		typedef typename allocator_type::const_pointer   	const_pointer;
+		typedef typename allocator_type::size_type       	size_type;
+
+		//tmp iterator defs
+		typedef typename std::random_access_iterator_tag	ra_it; // tmp name
+		typedef std::iterator<ra_it, value_type> 			iterator;
+		typedef std::iterator<ra_it , const value_type> 	const_iterator;
+		typedef std::reverse_iterator<iterator>          	reverse_iterator;
+		typedef std::reverse_iterator<const_iterator>    	const_reverse_iterator;
+		typedef typename std::iterator_traits<iterator>		i_t; // tmp name
+		typedef typename i_t::difference_type				difference_type;
+	};
+}
